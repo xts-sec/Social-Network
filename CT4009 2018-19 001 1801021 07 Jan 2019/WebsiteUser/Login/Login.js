00760cdabaa4f7d631106c4458c604bccf60ff96
Matt Cocklin <mnc2603@gmail.com>
2018-09-27 16:58:17 +0100

    created user authentication script

--- CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/Login/Login.js
+++ CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/Login/Login.js
@@ -1,14 +1,38 @@
-function Authenticate(){
+//array containing user objects
+var users = [
+    {
+    username: "test@test.com",
+    password: "password"
+    },
+    {
+    username: "me@me.co",
+    password: "mypass"
+    }
+]
 
-  if ((document.getElementById(Email) != "") && (document.getElementById(Password) != "")) {
-    var email = document.getElementById(Email).value
-    var password = document.getElementById(Password).value
-    alert("logging in")
-    window.location.href = "../CreateWallPost/CreateWallPost.html"
-    return true
-  }
-  else {
-    alert("one or more fields are blank")
-    return false
-  }
-};
+//function to authenticate users
+function Authenticate(){
+    
+    var loginEmail = document.getElementById("Email")
+    var loginPassword = document.getElementById("Password")
+    //debug
+    //console.log(loginEmail.value)
+    //console.log(loginPassword.value)
+    var i;
+    for (i = 0; i < users.length; i++){
+        //debug
+        //console.log(users[i].username)
+        //console.log(users[i].password)
+        if ((loginEmail.value == users[i].username) && (loginPassword.value == users[i].password)){
+            alert("logging in")
+            window.location.href = "../CreateWallPost/CreateWallPost.html"
+            return true
+        }
+        else{
+            //debug
+            //alert("Incorrect details")
+        }
+    }
+    alert("Incorrect details")
+    return false;
+}
