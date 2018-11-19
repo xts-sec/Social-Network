59ab4ea2c22c0f21a86021dafd0395f562698408
matt-cocklin <mnc2603@gmail.com>
2018-11-19 14:40:42 +0000

    re-did a bunch of stuff

--- CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/Login/Login.js
+++ CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/Login/Login.js
@@ -1,28 +1,3 @@
-//function to authenticate users
-function Authenticate(){
-  //gets user object array from local storage
-    var users = JSON.parse(localStorage.getItem("users"))
-
-    var loginEmail = document.getElementById("Email").value
-    var loginPassword = document.getElementById("Password").value
-    //debug
-    //console.log(loginEmail.value)
-    //console.log(loginPassword.value)
-    var i;
-    for (i = 0; i < users.length; i++){
-        //debug
-        //console.log(users[i].username)
-        //console.log(users[i].password)
-        if ((loginEmail == users[i].Email) && (loginPassword == users[i].Password)){
-            alert("logging in")
-            window.location.href = "../CreateWallPost/CreateWallPost.html"
-            return true
-        }
-        else{
-            //debug
-            //alert("Incorrect details")
-        }
-    }
-    alert("Incorrect details")
-    return false;
+function login(){
+  window.location = "../CreateWallPost/CreateWallPost.html"
 }
