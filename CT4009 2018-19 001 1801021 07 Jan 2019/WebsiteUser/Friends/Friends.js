1b24f2e8f7be3716ff81981cd1c6fd991f92cdb7
matt-cocklin <mnc2603@gmail.com>
2019-01-06 17:58:40 +0000

    started being able to add friends

--- CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/Friends/Friends.js
+++ CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/Friends/Friends.js
@@ -44,3 +44,51 @@ function loadUserList(){
     })
   }, function(){});
 }
+
+function addAsFriend(id){
+  myId = sessionStorage.getItem("UserID");
+  setDatabaseName('dbUsers', ['UsersObjectStore', 'PostsObjectStore', 'MessagesObjectStore', 'reportsObjectStore']);
+  setCurrObjectStoreName('UsersObjectStore');
+  startDB(function(){
+    //adds current user to selected user's friends
+    var result = selectOne(id, function(result){
+      return result;
+    })
+    if(result.friends){
+      result.friends.append(myId);
+    } else {
+      result.friends = [myId]
+    }
+    //adds selected user to current user's friends
+    var result = selectOne(myId, function(result){
+      return result;
+    })
+    result.friends.append(id);
+  }, function(){});
+}
+
+function removeAsFriend(id){
+  myId = sessionStorage.getItem("UserID");
+  setDatabaseName('dbUsers', ['UsersObjectStore', 'PostsObjectStore', 'MessagesObjectStore', 'reportsObjectStore']);
+  setCurrObjectStoreName('UsersObjectStore');
+  startDB(function(){
+    //removes current user from selected user's friends
+    var result = selectOne(id, function(result){
+      return result;
+    })
+    for( var i = 0; i < array.length-1; i++){
+      if ( array[i] === myId) {
+        arr.splice(i, 1);
+      }
+    }
+    //removes selected user from current user's friends
+    var result = selectOne(myId, function(result){
+      return result;
+    })
+    for( var i = 0; i < array.length-1; i++){
+      if ( array[i] === id) {
+        arr.splice(i, 1);
+      }
+    }
+  }, function(){});
+}
