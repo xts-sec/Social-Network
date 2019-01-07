d3f16fdd22daea595baa30d55e9a8b2189c939fc
matt-cocklin <mnc2603@gmail.com>
2019-01-07 14:44:14 +0000

    admin login exists

--- CT4009 2018-19 001 1801021 07 Jan 2019/Administrator/Login/Login.js
+++ CT4009 2018-19 001 1801021 07 Jan 2019/Administrator/Login/Login.js
@@ -1,3 +1,8 @@
 function login(){
+  if($("#txtPassword").val() == "pass"){
     window.location = "../ViewPostsSummary/ViewPostsSummary.html"
+  } else {
+    alert("Incorrect password");
+    $("#txtPassword").val() = "";
+  }
 }
