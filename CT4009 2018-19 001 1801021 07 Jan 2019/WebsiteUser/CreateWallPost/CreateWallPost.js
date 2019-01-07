0e2f9dce0858ac6e11b52965808b8571c34278d6
matt-cocklin <mnc2603@gmail.com>
2019-01-07 14:39:32 +0000

    only shows posts by friends

--- CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/CreateWallPost/CreateWallPost.js
+++ CT4009 2018-19 001 1801021 07 Jan 2019/WebsiteUser/CreateWallPost/CreateWallPost.js
@@ -9,20 +9,43 @@ function loadPosts(){
 }
 
 function publish(id, content, time){
+  var myId = sessionStorage.getItem("userID");
   setDatabaseName('dbUsers', ['UsersObjectStore', 'PostsObjectStore', 'MessagesObjectStore']);
 	setCurrObjectStoreName('UsersObjectStore');
   startDB(function () {
-    selectAll(function(results){
-      var name = "";
-      var len = results.length;
-      for(i = 0; i < len; i++){
-        if(results[i].id == id){
-          name = results[i].firstName + " " + results[i].lastName;
+    var showPost = false;
+    var myFriends = [];
+    selectOne(myId, function(result){
+      myFriends = result.friends
+      len = myFriends.length;
+      if(id == myId){
+        showPost = true;
+        alert(id + "is me");
+      }
+      if(len > 0){
+        for(i = 0; i < len; i++){
+          if(myFriends[i] == id){
+            showPost = true;
+            alert(id + "is a friend");
+          }
         }
       }
-      $("#postList").prepend("<li><p style='color: deepskyblue;'>- <b>" + name + "</b> @ " + time + "</p>" + content + "<hr></li>");
+      if(showPost == true){
+        selectAll(function(results){
+          var name = "";
+          var len = results.length;
+          for(i = 0; i < len; i++){
+            if(results[i].id == id){
+              name = results[i].firstName + " " + results[i].lastName;
+            }
+          }
+          $("#postList").prepend("<li><p style='color: deepskyblue;'>- <b>" + name + "</b> @ " + time + "</p>" + content + "<hr></li>");
+        })
+        $("#txtPostBody").val("");
+      } else {
+        alert(id + "is not me or a friend")
+      }
     })
-    $("#txtPostBody").val("");
   })
 }
 
